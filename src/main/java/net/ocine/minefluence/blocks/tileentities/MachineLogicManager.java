a5d6b7f31cc3d6e436ad2f144fdd7491f50fbbbf
Florian Stober <Flo.e3@gmx.de>
2014-10-31 00:08:50 +0100

    add more magic

--- src/main/java/net/ocine/minefluence/blocks/tileentities/MachineLogicManager.java
+++ src/main/java/net/ocine/minefluence/blocks/tileentities/MachineLogicManager.java
@@ -13,8 +13,13 @@ public class MachineLogicManager {
     public static AbstractMachineLogic dirtToDiamond = new TransformationLogic("Magie Mit Erde", 400,
             Arrays.asList(new Object[]{new ItemStack(Blocks.dirt)}), Arrays.asList(new Object[]{new ItemStack(Items.diamond)}));
 
+    public static AbstractMachineLogic magicIron = new TransformationLogic("Magie Mit Erde 2", 800,
+            Arrays.asList(new Object[]{new ItemStack(Blocks.dirt, 5), new ItemStack(Blocks.stone)}),
+            Arrays.asList(new Object[]{new ItemStack(Blocks.diamond_block), new ItemStack(Blocks.iron_bars)}));
+
     public static AbstractMachineLogic getApplicatableLogic(Machine machine){
         if(machine.getInputs() == 1 && machine.getOutputs() == 1 && machine.getWorkers() >= 1)return dirtToDiamond;
+        if(machine.getInputs() == 2 && machine.getOutputs() == 2 && machine.getWorkers() >= 1)return magicIron;
         return null;
     }
 }
