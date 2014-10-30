6a0dce9aaa5e68ddc0c2eb7e925e2b0367ce9443
nicosio2 <nico.gruendel@web.de>
2014-10-30 23:38:53 +0100

    Them bugs tho :(

--- src/main/java/net/ocine/minefluence/blocks/containers/InventoryContainer.java
+++ src/main/java/net/ocine/minefluence/blocks/containers/InventoryContainer.java
@@ -3,6 +3,7 @@ package net.ocine.minefluence.blocks.containers;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
@@ -42,33 +43,18 @@ public class InventoryContainer extends Container {
 
 	@Override
 	public ItemStack transferStackInSlot(EntityPlayer player, int slot) {
-		ItemStack stack = null;
-		Slot slotObject = (Slot) inventorySlots.get(slot);
-
-		if (slotObject != null && slotObject.getHasStack()) {
-			ItemStack stackInSlot = slotObject.getStack();
-			stack = stackInSlot.copy();
-
-			if (slot < 9) {
-				if (!this.mergeItemStack(stackInSlot, 0, 35, true)) {
-					return null;
-				}
-			} else if (!this.mergeItemStack(stackInSlot, 0, 9, false)) {
-				return null;
-			}
-
-			if (stackInSlot.stackSize == 0) {
-				slotObject.putStack(null);
-			} else {
-				slotObject.onSlotChanged();
-			}
-
-			if (stackInSlot.stackSize == stack.stackSize) {
-				return null;
-			}
-			slotObject.onPickupFromSlot(player, stackInSlot);
+		return null;
+	}
+	
+	protected class OutputSlot extends Slot {
+		public OutputSlot(IInventory p_i1824_1_, int p_i1824_2_, int p_i1824_3_, int p_i1824_4_) {
+			super(p_i1824_1_, p_i1824_2_, p_i1824_3_, p_i1824_4_);
 		}
-		return stack;
+		
+		@Override
+		public boolean isItemValid(ItemStack stack) {
+			return false;
+		}
 	}
 
 }
