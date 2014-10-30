c7b7d0be2a6e09249fdbcf5d29818cf1cad9180a
Florian Stober <Flo.e3@gmx.de>
2014-10-30 17:10:50 +0100

    make Core implement IMachinePart

--- src/main/java/net/ocine/minefluence/blocks/tileentities/TileEntityCore.java
+++ src/main/java/net/ocine/minefluence/blocks/tileentities/TileEntityCore.java
@@ -2,7 +2,7 @@ package net.ocine.minefluence.blocks.tileentities;
 
 import net.minecraft.tileentity.TileEntity;
 
-public class TileEntityCore extends TileEntity implements Machine {
+public class TileEntityCore extends TileEntity implements Machine, IMachinePart {
 	private int counter = 0;
 	@Override
 	public void updateEntity() {
@@ -15,6 +15,23 @@ public class TileEntityCore extends TileEntity implements Machine {
 	
 	public Machine getMachine(){
         return this;
+    }
+
+    @Override
+    public boolean isPartOfMachine() {
+        // of cause it is - we are the machine
+        return true;
+    }
+
+    @Override
+    public boolean assignToMachine(Machine machine, boolean force) {
+        // NO THIS CAN'T BE - CRY
+        throw new RuntimeException("cry");
+    }
+
+    @Override
+    public boolean removeFromMachine() {
+        return true;
     }
 
     @Override
