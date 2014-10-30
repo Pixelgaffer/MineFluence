3eed824f1f1714276d2ea79d3dd539a619c00a4d
Florian Stober <Flo.e3@gmx.de>
2014-10-30 16:54:02 +0100

    machines structure

--- src/main/java/net/ocine/minefluence/blocks/tileentities/MachinePartBase.java
+++ src/main/java/net/ocine/minefluence/blocks/tileentities/MachinePartBase.java
@@ -1,17 +1,7 @@
 package net.ocine.minefluence.blocks.tileentities;
 
-<<<<<<< HEAD
 public class MachinePartBase implements IMachinePart{
     private Machine machine;
-
-=======
-import javax.annotation.Nullable;
-
-public class MachinePartBase implements IMachinePart{
-    private Machine machine;
-
-    @Nullable
->>>>>>> origin/master
     @Override
     public Machine getMachine() {
         return machine;
