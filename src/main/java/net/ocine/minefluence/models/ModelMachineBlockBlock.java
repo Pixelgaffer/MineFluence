ef1b48df71c79b24142d153e8a2463753f1ecfb8
nicosio2 <nico.gruendel@web.de>
2014-11-02 12:24:01 +0100

    updated models

---
+++ src/main/java/net/ocine/minefluence/models/ModelMachineBlockBlock.java
@@ -0,0 +1,40 @@
+package net.ocine.minefluence.models;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+
+public class ModelMachineBlockBlock extends ModelBase {
+	private ModelRenderer shape1;
+
+	public ModelMachineBlockBlock() {
+		textureWidth = 64;
+		textureHeight = 32;
+
+		shape1 = new ModelRenderer(this, 0, 0);
+		shape1.addBox(0F, 0F, 0F, 14, 14, 14);
+		shape1.setRotationPoint(-7F, 8F, -7F);
+		shape1.setTextureSize(64, 32);
+		shape1.mirror = true;
+		setRotation(shape1, 0F, 0F, 0F);
+	}
+
+	public void render(Entity entity, float f, float f1, float f2, float f3,
+			float f4, float f5) {
+		super.render(entity, f, f1, f2, f3, f4, f5);
+		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+		shape1.render(f5);
+	}
+
+	private void setRotation(ModelRenderer model, float x, float y, float z) {
+		model.rotateAngleX = x;
+		model.rotateAngleY = y;
+		model.rotateAngleZ = z;
+	}
+
+	public void setRotationAngles(float f, float f1, float f2, float f3,
+			float f4, float f5, Entity entity) {
+		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+	}
+
+}
--- src/main/java/net/ocine/minefluence/models/ModelMachineBlockBorder.java
+++ src/main/java/net/ocine/minefluence/models/ModelMachineBlockBorder.java
@@ -4,7 +4,8 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class ModelMachineBlock extends ModelBase {
+public class ModelMachineBlockBorder extends ModelBase {
+	
 	private ModelRenderer shape1;
 	private ModelRenderer shape2;
 	private ModelRenderer shape3;
@@ -17,90 +18,83 @@ public class ModelMachineBlock extends ModelBase {
 	private ModelRenderer shape10;
 	private ModelRenderer shape11;
 	private ModelRenderer shape12;
-	private ModelRenderer shape13;
 
-	public ModelMachineBlock() {
+	public ModelMachineBlockBorder() {
 		textureWidth = 128;
 		textureHeight = 128;
 
-		shape1 = new ModelRenderer(this, 35, 0);
-		shape1.addBox(0F, 0F, 0F, 14, 14, 14);
-		shape1.setRotationPoint(-7F, 9F, -7F);
-		shape1.setTextureSize(128, 128);
+		shape1 = new ModelRenderer(this, 0, 0);
+		shape1.addBox(0F, 0F, 0F, 1, 1, 16);
+		shape1.setRotationPoint(-8F, 8F, -8F);
+		shape1.setTextureSize(64, 32);
 		shape1.mirror = true;
 		setRotation(shape1, 0F, 0F, 0F);
 		shape2 = new ModelRenderer(this, 0, 0);
 		shape2.addBox(0F, 0F, 0F, 1, 1, 16);
-		shape2.setRotationPoint(-8F, 8F, -8F);
-		shape2.setTextureSize(128, 128);
+		shape2.setRotationPoint(7F, 8F, -8F);
+		shape2.setTextureSize(64, 32);
 		shape2.mirror = true;
 		setRotation(shape2, 0F, 0F, 0F);
 		shape3 = new ModelRenderer(this, 0, 0);
 		shape3.addBox(0F, 0F, 0F, 1, 1, 16);
-		shape3.setRotationPoint(7F, 8F, -8F);
-		shape3.setTextureSize(128, 128);
+		shape3.setRotationPoint(7F, 23F, -8F);
+		shape3.setTextureSize(64, 32);
 		shape3.mirror = true;
 		setRotation(shape3, 0F, 0F, 0F);
 		shape4 = new ModelRenderer(this, 0, 0);
 		shape4.addBox(0F, 0F, 0F, 1, 1, 16);
-		shape4.setRotationPoint(7F, 23F, -8F);
-		shape4.setTextureSize(128, 128);
+		shape4.setRotationPoint(-8F, 23F, -8F);
+		shape4.setTextureSize(64, 32);
 		shape4.mirror = true;
 		setRotation(shape4, 0F, 0F, 0F);
-		shape5 = new ModelRenderer(this, 0, 0);
-		shape5.addBox(0F, 0F, 0F, 1, 1, 16);
-		shape5.setRotationPoint(-8F, 23F, -8F);
-		shape5.setTextureSize(128, 128);
+		shape5 = new ModelRenderer(this, 0, 19);
+		shape5.addBox(0F, 0F, 0F, 14, 1, 1);
+		shape5.setRotationPoint(-7F, 23F, -8F);
+		shape5.setTextureSize(64, 32);
 		shape5.mirror = true;
 		setRotation(shape5, 0F, 0F, 0F);
 		shape6 = new ModelRenderer(this, 0, 19);
 		shape6.addBox(0F, 0F, 0F, 14, 1, 1);
-		shape6.setRotationPoint(-7F, 23F, -8F);
-		shape6.setTextureSize(128, 128);
+		shape6.setRotationPoint(-7F, 8F, 7F);
+		shape6.setTextureSize(64, 32);
 		shape6.mirror = true;
 		setRotation(shape6, 0F, 0F, 0F);
 		shape7 = new ModelRenderer(this, 0, 19);
 		shape7.addBox(0F, 0F, 0F, 14, 1, 1);
-		shape7.setRotationPoint(-7F, 8F, 7F);
-		shape7.setTextureSize(128, 128);
+		shape7.setRotationPoint(-7F, 23F, 7F);
+		shape7.setTextureSize(64, 32);
 		shape7.mirror = true;
 		setRotation(shape7, 0F, 0F, 0F);
 		shape8 = new ModelRenderer(this, 0, 19);
 		shape8.addBox(0F, 0F, 0F, 14, 1, 1);
-		shape8.setRotationPoint(-7F, 23F, 7F);
-		shape8.setTextureSize(128, 128);
+		shape8.setRotationPoint(-7F, 8F, -8F);
+		shape8.setTextureSize(64, 32);
 		shape8.mirror = true;
 		setRotation(shape8, 0F, 0F, 0F);
-		shape9 = new ModelRenderer(this, 0, 19);
-		shape9.addBox(0F, 0F, 0F, 14, 1, 1);
-		shape9.setRotationPoint(-7F, 8F, -8F);
-		shape9.setTextureSize(128, 128);
+		shape9 = new ModelRenderer(this, 0, 22);
+		shape9.addBox(0F, 0F, 0F, 1, 14, 1);
+		shape9.setRotationPoint(7F, 9F, -8F);
+		shape9.setTextureSize(64, 32);
 		shape9.mirror = true;
 		setRotation(shape9, 0F, 0F, 0F);
 		shape10 = new ModelRenderer(this, 0, 22);
 		shape10.addBox(0F, 0F, 0F, 1, 14, 1);
-		shape10.setRotationPoint(7F, 9F, -8F);
-		shape10.setTextureSize(128, 128);
+		shape10.setRotationPoint(-8F, 9F, -8F);
+		shape10.setTextureSize(64, 32);
 		shape10.mirror = true;
 		setRotation(shape10, 0F, 0F, 0F);
 		shape11 = new ModelRenderer(this, 0, 22);
 		shape11.addBox(0F, 0F, 0F, 1, 14, 1);
-		shape11.setRotationPoint(-8F, 9F, -8F);
-		shape11.setTextureSize(128, 128);
+		shape11.setRotationPoint(-8F, 9F, 7F);
+		shape11.setTextureSize(64, 32);
 		shape11.mirror = true;
 		setRotation(shape11, 0F, 0F, 0F);
-		shape12 = new ModelRenderer(this, 0, 22);
+		shape12 = new ModelRenderer(this, 0, 21);
 		shape12.addBox(0F, 0F, 0F, 1, 14, 1);
-		shape12.setRotationPoint(-8F, 9F, 7F);
-		shape12.setTextureSize(128, 128);
+		shape12.setRotationPoint(7F, 9F, 7F);
+		shape12.setTextureSize(64, 32);
 		shape12.mirror = true;
 		setRotation(shape12, 0F, 0F, 0F);
-		shape13 = new ModelRenderer(this, 0, 21);
-		shape13.addBox(0F, 0F, 0F, 1, 14, 1);
-		shape13.setRotationPoint(7F, 9F, 7F);
-		shape13.setTextureSize(128, 128);
-		shape13.mirror = true;
-		setRotation(shape13, 0F, 0F, 0F);
 	}
 
 	public void render(Entity entity, float f, float f1, float f2, float f3,
@@ -119,18 +113,17 @@ public class ModelMachineBlock extends ModelBase {
 		shape10.render(f5);
 		shape11.render(f5);
 		shape12.render(f5);
-		shape13.render(f5);
 	}
 
 	private void setRotation(ModelRenderer model, float x, float y, float z) {
 		model.rotateAngleX = x;
 		model.rotateAngleY = y;
 		model.rotateAngleZ = z;
-	} 
+	}
 
 	public void setRotationAngles(float f, float f1, float f2, float f3,
 			float f4, float f5, Entity entity) {
-		super.setRotationAngles(f, f1, f2, f3, f4, f5,entity);
+		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
 	}
 
 }
