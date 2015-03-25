d1b5bdc7c264a4ea255f6aa56e959f2e38172f79
Florian Stober <Flo.e3@gmx.de>
2015-03-25 08:46:54 +0100

    make TileEntityCooler serializable

--- src/main/java/net/ocine/minefluence/blocks/tileentities/TileEntityCooler.java
+++ src/main/java/net/ocine/minefluence/blocks/tileentities/TileEntityCooler.java
@@ -1,5 +1,6 @@
 package net.ocine.minefluence.blocks.tileentities;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.gui.IUpdatePlayerListBox;
 import net.ocine.minefluence.blocks.MachineBlocks.Machines;
 
@@ -7,6 +8,10 @@ public class TileEntityCooler extends TileEntityMachinePart implements IUpdatePl
 	
 	private int coolAmount;
 	private Machines type;
+
+	public TileEntityCooler() {
+		type = Machines.FAN;
+	}
 	
 	public TileEntityCooler(Machines type, int coolAmount) {
 		this.coolAmount = coolAmount;
@@ -23,5 +28,20 @@ public class TileEntityCooler extends TileEntityMachinePart implements IUpdatePl
 		if(!worldObj.isRemote && isPartOfMachine()) {
 			getMachine().cool(coolAmount);
 		}
+	}
+
+	@Override public void readFromNBT(NBTTagCompound p_145839_1_) {
+		super.readFromNBT(p_145839_1_);
+		coolAmount = p_145839_1_.getInteger("coolAmount");
+		String coolerType = p_145839_1_.getString("coolerType");
+		if(coolerType != null && !coolerType.isEmpty()){
+			type = Machines.valueOf(coolerType);
+		}
+	}
+
+	@Override public void writeToNBT(NBTTagCompound p_145841_1_) {
+		super.writeToNBT(p_145841_1_);
+		p_145841_1_.setInteger("coolAmount", coolAmount);
+		p_145841_1_.setString("coolerType", type.getName());
 	}
 }
